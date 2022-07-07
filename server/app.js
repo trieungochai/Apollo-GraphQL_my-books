17f24583d219d6a94e6ae425e82ebbb9482d9c81
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-07-07 21:45:33 +0700

    Connect to MongoDB

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -7,6 +8,25 @@ const PORT = process.env.PORT || 4000;
 // Load schema & resolvers
 const typeDefs = require("./schema/schema");
 const resolvers = require("./resolver/resolver");
+
+// Connect to MongoDB
+const uri =
+  "mongodb+srv://haitn:pjvywbq1pdw4gjVk@my-books.bw0tg.mongodb.net/?retryWrites=true&w=majority";
+const connectDB = async () => {
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
+
+connectDB();
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
