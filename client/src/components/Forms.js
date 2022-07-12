9a61c3545d528aeabc41e01c5e80060678d956ad
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-07-12 11:34:33 +0700

    Getting the newBook data on submit

--- client/src/components/Forms.js
+++ client/src/components/Forms.js
@@ -1,39 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Col, Form, Row } from "react-bootstrap";
 
 import { useQuery } from "@apollo/client";
 import { getAllAuthors } from "../graphql-client/queries";
 
 const Forms = () => {
+  const [newBook, setNewBook] = useState({
+    title: "",
+    genre: "",
+    authorId: "",
+  });
+
+  const onInputChange = (event) => {
+    setNewBook({
+      ...newBook,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(newBook);
+  };
+
   const { loading, error, data } = useQuery(getAllAuthors);
   if (error) {
     console.log(error.message);
     return <p>Error loading authors!</p>;
   }
-  console.log("🚀 ~ file: Forms.js ~ line 9 ~ Forms ~ data", data);
 
   return (
     <Row>
       <Col>
-        <Form>
+        <Form onSubmit={onSubmit}>
           <Form.Group>
-            <Form.Control type="text" placeholder="Book title" />
+            <Form.Control
+              type="text"
+              placeholder="Book title"
+              name="title"
+              onChange={onInputChange}
+              value={newBook.title}
+            />
           </Form.Group>
           <Form.Group>
-            <Form.Control type="text" placeholder="Book genre" />
+            <Form.Control
+              type="text"
+              placeholder="Book genre"
+              name="genre"
+              onChange={onInputChange}
+              value={newBook.genre}
+            />
           </Form.Group>
           <Form.Group>
             {loading ? (
               <p>Loading authors....</p>
             ) : (
-              <Form.Select type="select" defaultValue="Select author">
-                <option disabled>Select author</option>
+              <Form.Control
+                as="select"
+                name="authorId"
+                onChange={onInputChange}
+                value={newBook.authorId}
+              >
+                <option value="" disabled>
+                  Select author
+                </option>
                 {data.authors.map((author) => (
                   <option key={author.id} value={author.id}>
                     {author.name}
                   </option>
                 ))}
-              </Form.Select>
+              </Form.Control>
             )}
           </Form.Group>
           <Button className="float-right" variant="info" type="submit">
